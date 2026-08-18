18056f6339b37d844213e19b7f6cb034795a7d4e
schleidenalcius <schleiden-a@outlook.fr>
2026-08-18 16:29:21 -0400

    Ajout de filter sur les images. Jeu complètement fonctionnel.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let sonJoue = document.querySelector(".jeu");
 let chemin = "Sons/Jeu/";
 let btnTester = document.querySelector("button");
 let btnNotes = document.querySelectorAll(".note");
+let leResultat= document.querySelector("#leResultat");
+let imgNote = document.querySelector("#imgNote");
+let cheminImgNote = "Images/Jeu/";
+let rentrerUnePremièreFois = 0;
 
 /*Déclarations de mes fonctions*/
 
@@ -12,50 +16,65 @@ function choisirUnSon(){
     switch (numeroAleatoire){
         case 1:
             chemin = chemin + "Do.mp3"
+            cheminImgNote = cheminImgNote+"Do.png";
         break;
         case 2:
             chemin = chemin + "DoD.mp3"
+            cheminImgNote = cheminImgNote+"DoD.png";
         break;
         case 3:
             chemin = chemin + "Re.mp3"
+            cheminImgNote = cheminImgNote+"Re.png";
         break;
         case 4:
             chemin = chemin + "ReD.mp3"
+            cheminImgNote = cheminImgNote+"ReD.png";
         break;
         case 5:
             chemin = chemin + "Mi.mp3"
+            cheminImgNote = cheminImgNote+"Mi.png";
         break;
         case 6:
             chemin = chemin + "Fa.mp3"
+            cheminImgNote = cheminImgNote+"Fa.png";
         break;
         case 7:
             chemin = chemin + "FaD.mp3"
+            cheminImgNote = cheminImgNote+"FaD.png";
         break;
         case 8:
             chemin = chemin + "Sol.mp3"
+            cheminImgNote = cheminImgNote+"Sol.png";
         break;
         case 9:
             chemin = chemin + "SolD.mp3"
+            cheminImgNote = cheminImgNote+"SolD.png";
         break;
         case 10:
             chemin = chemin + "La.mp3"
+            cheminImgNote = cheminImgNote+"La.png";
         break;
         case 11:
             chemin = chemin + "LaD.mp3"
+            cheminImgNote = cheminImgNote+"LaD.png";
         break;
         case 12:
             chemin = chemin + "Si.mp3"
+            cheminImgNote = cheminImgNote+"Si.png";
         break;
     }
+   
+
     modifierSonJoue();
 }
 
 /*Fonction qui permet de changer la source de la balise audio*/
 function modifierSonJoue(){
     sonJoue.src= chemin;
-    console.log(sonJoue);
     sonJoue.load();
+    if(rentrerUnePremièreFois>0){
     sonJoue.play();
+    }
 }
 
 choisirUnSon();
@@ -66,18 +85,27 @@ function testerSons(){
     btnNotes.forEach((btnNote)=>{
         btnNote.addEventListener("click",(event)=>{
             event.preventDefault();
-            let lanote = btnNote.textContent;
-            console.log(lanote);
-            let laNoteCorrigee = lanote.replace("#","D");
-            console.log(`le hashtag remplacé :`+laNoteCorrigee);
-            if (chemin.includes(lanote)){
-                console.log("Réussi");
+            let lanote = btnNote.textContent;         
+            let laNoteCorrigee = lanote.replace("#","D"); 
+            const nomFichier = chemin.split("/");   
+            const nomFichierNettoye=nomFichier[2].split(".")[0];
+            if (laNoteCorrigee===nomFichierNettoye){
+                leResultat.textContent="Réussi";
+                imgNote.src=cheminImgNote;
             }else{
-                console.log("Erreur!! Recommencer");
-
+               leResultat.textContent="Vous y êtes presque! Recommencer";
             }
         });
     });
 }
 
 testerSons();
+btnTester.addEventListener("click", ()=>{
+   chemin = "Sons/Jeu/";
+   cheminImgNote = "Images/Jeu/";
+   imgNote.src="";
+   leResultat.textContent="Jouez!";
+   rentrerUnePremièreFois=1;
+   choisirUnSon();
+   testerSons();
+});
